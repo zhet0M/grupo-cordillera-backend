ddabb9c89c1ab0010e334a41cf8cba4e90472b91
Matias <matiasmedinamedina538@gmail.com>
2026-06-28 20:39:59 -0400

    fix: intentod de fix de metabase

--- authentication/src/main/java/com/autenticacion/authentication/config/SecurityConfig.java
+++ authentication/src/main/java/com/autenticacion/authentication/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/auth/login", "/auth/registro").permitAll()
                 .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**", "/webjars/**").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
                 .requestMatchers("/auth/admin/**").hasAnyAuthority("SUPER_ADMIN", "ADMIN_USUARIOS")
                 .anyRequest().authenticated()
             )
